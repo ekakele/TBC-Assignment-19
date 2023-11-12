695f89ab5e274f9f45d4d046643f9598e62b9ea0
ekakele <ekaakelenjeridze@gmail.com>
2023-11-13 01:14:56 +0400

    Initial Commit

---
+++ Assignment-19/Scenes/BounceAnimationViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ViewController.swift
+//  Assignment-19
+//
+//  Created by Eka Kelenjeridze on 12.11.23.
+//
+
+import UIKit
+
+final class BounceAnimationViewController: UIViewController {
+    // MARK: - Properties
+    private let TBCLabel: UILabel = {
+        let label = UILabel()
+        label.text = "TBC IT Academy"
+        label.textAlignment = .center
+        label.textColor = UIColor(red: 0.38, green: 0.74, blue: 0.81, alpha: 1.00)
+        label.font = UIFont.systemFont(ofSize: 22, weight: .bold)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    // MARK: - ViewLifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupBackground()
+        setupLabel()
+        
+        animateBounce(for: TBCLabel)
+        animateFade(for: TBCLabel)
+    }
+    
+    // MARK: - Private Methods
+    private func setupBackground() {
+        view.backgroundColor = .systemBackground
+    }
+    
+    private func setupLabel() {
+        view.addSubview(TBCLabel)
+        
+        NSLayoutConstraint.activate([
+            TBCLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            TBCLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+    
+    private func animateBounce(for label: UILabel) {
+        UIView.animate(withDuration: 2.2, animations: {
+            label.frame.origin.y += 50
+        }) { _ in
+            UIView.animate(withDuration: 2.2, animations: {
+                label.frame.origin.y -= 50
+            }) { _ in
+                self.animateBounce(for: label)
+            }
+        }
+    }
+    
+    private func animateFade(for label: UILabel) {
+        UIView.animate(withDuration: 2.2, animations: {
+            label.alpha = 0.0
+        }) { _ in
+            UIView.animate(withDuration: 2.2) {
+                label.alpha = 1.0
+            } completion: { _ in
+                self.animateFade(for: label)
+            }
+        }
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,11 @@
+# TBC-Assignment-19
+TBC IT Academy | iOS development Course | Module 2 | Assignment #19
+
+## Assignment concept
+
+The assignment is designed to introduce UIView Animations by means of altering the position of UI elements through move/bounce animations and adjusting their opacity through fade animations.
+
+The assignment covers the following topics: 
+* UIView Animations
+
+> The deadline of this task is: 13/11/2023, 10:00. 
